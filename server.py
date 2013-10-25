ad56d06fe808f2e597d312837c08c2a254d63bfb
Joe Cross <joe.mcross@gmail.com>
2013-10-24 17:56:07 -0700

    Add in-memory hit count

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from bottle import route, static_file, run, template
 from multihost_example import config, util
 
 import socket
+fact_page_loads = 0
 hostname = socket.gethostname()
 facts = util.load_file('facts').split('\n')
 pics = list(util.load_file_config({}, 'static/sources'))
@@ -18,8 +19,14 @@ def static(filename):
 
 @route('/fact')
 def fact():
+    global fact_page_loads
+    fact_page_loads += 1
     fact = random.choice(facts)
     src = '/static/' + random.choice(pics) + '.jpg'
-    return template(layout, src=src, fact=fact, host=hostname)
+    return template(layout, src=src, fact=fact, host=hostname, hits=fact_page_loads)
+
+@route('/hits')
+def hits():
+    return fact_page_loads
 
 run(host=config['HOST'], port=config['PORT'], reloader=True)
