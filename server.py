0803b8de2587a442f9f816d88dc405e3555c74a9
Joe Cross <joe.mcross@gmail.com>
2013-10-24 17:13:24 -0700

    Clean up views

--- server.py
+++ server.py
@@ -4,32 +4,28 @@ from multihost_example import config, util
 
 import socket
 hostname = socket.gethostname()
-
-@route('/hello/<name>')
-def index(name='World'):
-    return template('<b>Hello {{name}}</b>! (sent from my {{hostname}})',
-        name=name, hostname=hostname)
+facts = util.load_file('facts').split('\n')
+pics = list(util.load_file_config({}, 'static/sources'))
 
 @route('/ping')
 def ping():
     return 'HEALTHY'
 
-facts = util.load_file('facts')
-facts = facts.split('\n')
-
-pics = list(util.load_file_config({}, 'static/sources'))
-
 @route('/static/<filename>')
-def server_static(filename):
-    print "Trying to serve {}".format(filename)
+def static(filename):
     return static_file(filename, root=util.abs_path('static'))
 
-@route('/random')
-def random_fact():
+@route('/fact')
+def fact():
     fact = random.choice(facts)
-    img = random.choice(pics) + '.jpg'
-    src = '/static/' + img
-    layout = '<img src="{{src}}"></img><h1>Fact</h1>\n<p>{{fact}}</p>\n<p><h3>Brought to you by</h3>\n<p>{{host}}</p>'
+    src = '/static/' + random.choice(pics) + '.jpg'
+    layout = """
+    <img src="{{src}}"></img>
+    <h1>Fact</h1>
+    <p>{{fact}}</p>
+    <h3>Brought to you by</h3>
+    <p>{{host}}</p>'
+    """
     return template(layout, src=src, fact=fact, host=hostname)
 
 run(host=config['HOST'], port=config['PORT'], reloader=True)
