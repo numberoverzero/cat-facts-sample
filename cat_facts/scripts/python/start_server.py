62a6e1678c5758c4d8de1b529456ea9f79e14da8
Joe Cross <joe.mcross@gmail.com>
2013-10-25 17:39:38 -0700

    Move to async server: gevent

--- cat_facts/scripts/python/start_server.py
+++ cat_facts/scripts/python/start_server.py
@@ -1,3 +1,5 @@
+from gevent import monkey
+monkey.patch_all()
 import bottle
 from cat_facts import app
-bottle.run(app, server='paste', host=app.config['HOST'], port=app.config['PORT'], reloader=app.config['DEBUG'])
+bottle.run(app, server='gevent', host=app.config['HOST'], port=app.config['PORT'], reloader=app.config['DEBUG'])
